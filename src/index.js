b0de6463bc40e986c1959b9405d22b148b53ff0a
Jonathan Cheng <jcheng461@gmail.com>
2022-07-16 12:27:09 -0700

    convert unix time to standard US. fix 404 errors from null api call responses

--- src/index.js
+++ src/index.js
@@ -25,19 +25,60 @@ $(document).ready(function () {
     if (response.bikes) {
       for (let i = 0; i < response.bikes.length; i++) {
         // Row 1: Number / Title of Post
-        let title = `<h4>${i + 1}. ${response.bikes[i].title}</h4>`;
+        let title = "(none provided)";
+        if (response.bikes[i].title != null) {
+          title = `<h4>${i + 1}. ${response.bikes[i].title}</h4>`;
+        }
         $(".showResults").append(`<br><div class="row">${title}</div>`);
         // Row 2: (left side) - Picture
-        let picture = `<a href="${response.bikes[i].url}"><img src="${response.bikes[i].thumb}" alt="${response.bikes[i].description}"></a>`;
+        let picture = "(none provided)";
+        if (
+          response.bikes[i].url != null &&
+          response.bikes[i].thumb != null &&
+          response.bikes[i].description != null
+        ) {
+          picture = `<a href="${response.bikes[i].url}"><img src="${response.bikes[i].thumb}" alt="${response.bikes[i].description}"></a>`;
+        }
         // Row 2: (right side) - Details
-        let details = `<h5>Details</h5><ul><li>Date Stolen: ${response.bikes[i].date_stolen}</li><li>Description: ${response.bikes[i].description}</li><li>Manufacturer Name: ${response.bikes[i].manufacturer_name}</li><li>Year: ${response.bikes[i].year}</li></ul>`;
+        let date = "(none provided)";
+        if (response.bikes[i].date_stolen != null) {
+          date = convertUnix(response.bikes[i].date_stolen);
+        }
+        let description = "(none provided)";
+        if (response.bikes[i].description != null) {
+          description = response.bikes[i].description;
+        }
+        let manufacturer = "(none provided)";
+        if (response.bikes[i].manufacturer_name != null) {
+          manufacturer = response.bikes[i].manufacturer_name;
+        }
+        let year = "(none provided)";
+        if (response.bikes[i].year != null) {
+          year = response.bikes[i].year;
+        }
+        let details = `
+        <h5>Details</h5>
+          <ul>
+            <li>Date Stolen: ${date}</li>
+            <li>Description: ${description}</li>
+            <li>Manufacturer Name: ${manufacturer}</li>
+            <li>Year: ${year}</li>
+          </ul>`;
         // Row 2: Display to HTML
         $(".showResults").append(
-          `<div class="row"><div class="col-md-6">${picture}</div><div class="col-md-6">${details}</div></div>`
+          `<div class="row">
+            <div class="col-md-6">${picture}</div>
+            <div class="col-md-6">${details}</div>
+          </div>`
         );
       }
     } else {
       $(".showErrors").text(`There was an error: ${response.message}`);
     }
+  }
+
+  function convertUnix(unix) {
+    const date = new Date(unix * 1000);
+    return date.toLocaleDateString("en-US");
   }
 });
